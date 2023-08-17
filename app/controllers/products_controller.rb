699122d4431ffbb35f269f6630c89bc8a9b0fbb1
Franci <francesca1999@yahoo.com>
2023-08-17 15:57:59 +0300

    add vegetarian field when creating a product

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -41,6 +41,6 @@ class ProductsController < ApplicationController
 
   private
     def product_params
-      params.require(:product).permit(:name, :price, :description, :image, :category)
+      params.require(:product).permit(:name, :price, :vegetarian, :description, :image, :category)
     end
 end
