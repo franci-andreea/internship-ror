93b37ba393a39868349f1dc6be0b8f74ffd9952b
Franci <francesca1999@yahoo.com>
2023-08-17 16:09:14 +0300

    remove comments from seeds.rb

--- db/seeds.rb
+++ db/seeds.rb
@@ -52,22 +52,12 @@ crepes_with_nutella = Product.create(
 crepes_with_nutella.image.attach(io: File.open(Rails.root.join('db/images/crepes-with-nutella.png')), filename: 'crepes-with-nutella.png')
 puts("CREPES WITH NUTELLA CREATED AND SUCCESSFULLY ADDED TO THE DATABASE, YAY!")
 
-
-# resource_type = "image"
-# type = "upload"
-# version = "v1_1"
-# public_id = "0kytrnmlvraet2jp3hlfdyk4zv86"
-# format = "png"
-# signature = Cloudinary::Utils.api_sign_request({:public_id=>public_id}, Cloudinary.config.api_secret)
-# pizza_california_image = "#{resource_type}/#{type}/v#{version}/#{public_id}.#{format}##{signature}"
-
 pizza_california = Product.create(
   name: 'Pizza California',
   description: "Description for Pizza California",
   price: 12.70,
   vegetarian: false,
   category: 2,
-  # image: pizza_california_image
 )
 pizza_california.image.attach(io: File.open(Rails.root.join('db/images/california-pizza.png')), filename: 'california-pizza.png')
 puts("PIZZA CALIFORNIA CREATED AND SUCCESSFULLY ADDED TO THE DATABASE, YAY!")
