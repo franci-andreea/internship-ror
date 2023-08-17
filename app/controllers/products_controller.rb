162915935ae6d8b7aa0bca2c0230655ad3472959
Franci <francesca1999@yahoo.com>
2023-08-17 11:01:23 +0300

    implement redirect to edit page for products

--- app/controllers/products_controller.rb
+++ app/controllers/products_controller.rb
@@ -2,4 +2,15 @@ class ProductsController < ApplicationController
   def index
     @products = Product.all
   end
+
+  def edit
+    puts "params[:id] = #{params[:id]}"
+    @product = Product.find(params[:id])
+  end
+
+  def destroy
+    Product.find(params[:id]).destroy
+    flash[:success] = "Product with #{params[:id]} successfully deleted!"
+    redirect_to products_path
+  end
 end
