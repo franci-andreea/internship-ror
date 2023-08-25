9a1423ec8e1c79197156f6d7b5dcf48c42268ded
Franci <francesca1999@yahoo.com>
2023-08-25 15:06:19 +0300

    implement test for DELETE product request

--- spec/requests/api/v1/products_controller_spec.rb
+++ spec/requests/api/v1/products_controller_spec.rb
@@ -5,6 +5,16 @@ RSpec.describe 'Products', type: :request do
   let(:admin) { create(:user, :is_admin) }
   let(:token) { JsonWebToken.encode(user_id: admin.id) }
   let(:headers) { {'Authorization' => "Bearer #{token}"} }
+  let(:product_to_delete) { 
+    Product.create(
+      id: 10, 
+      name: "Product To Be Deleted", 
+      price: 19.99, 
+      vegetarian: false,
+      category: "entree",
+      description: "description test"
+    ) 
+  }
 
   let(:product_params) do
     {
@@ -30,6 +40,19 @@ RSpec.describe 'Products', type: :request do
     end
   end
 
+  describe 'DELETE /api/v1/products/:id' do
+    context 'user has admin role' do
+      before do
+        token
+      end
+
+      it 'should delete the product with id 10' do
+        expect { delete "/api/v1/products/10", params: {id: 10}, headers: headers }.to change(Product, :count).by(-1)
+        expect(response).to have_http_status(:ok)
+      end
+    end
+  end
+
   describe 'GET /api/v1/products' do
     subject { get '/api/v1/products', headers: headers }
 
@@ -42,7 +65,6 @@ RSpec.describe 'Products', type: :request do
       end
 
       it 'should return all products' do
-        puts JSON.parse(response.body)
         expect(response).to have_http_status(:success)
         expect(JSON.parse(response.body).size).to eq(5)
         expect(JSON.parse(response.body)).to all(include('id', 'name', 'price', 'vegetarian', 'category', 'description'))
